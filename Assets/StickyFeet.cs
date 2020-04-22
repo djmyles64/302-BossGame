a45dca2d680292b6765e160c647b09bead24958d
Danny Myles <mylesd2@ferris.edu>
2020-04-22 13:11:56 -0400

    Added quadruped

---
+++ Assets/StickyFeet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyFeet : MonoBehaviour
+{
+    Vector3 plantedPositionLast;
+    Vector3 plantedPositionNext;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        CheckIfCanMoveFoot();
+        AnimateFoot();
+
+    }
+    void AnimateFoot()
+    {
+
+    }
+    void CheckIfCanMoveFoot()
+    {
+        DoRaycast();
+    }
+    void DoRaycast ()
+    {
+
+    }
+}
