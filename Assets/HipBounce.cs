82926b88c3a2e3a81334380d1d8ca6408825713b
Danny Myles <mylesd2@ferris.edu>
2020-04-29 02:08:26 -0400

    Fixed hip bounce

---
+++ Assets/HipBounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HipBounce : MonoBehaviour
+{
+    public float sinwaveSpeed = 7;
+    public float sinwaveOffset = 0;
+    public float sinwaveAmp = .001f;
+
+    public float bounceAmplitude = .001f;
+    public float rockingAmplitude = 1f;
+
+    Vector3 startingPosition;
+    Quaternion startingRotation;
+
+    void Start()
+    {
+        startingPosition = transform.localPosition;
+        startingRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float bounceAmount = Mathf.Sin((Time.time + sinwaveOffset * Mathf.PI) * 7);
+
+        transform.localPosition = startingPosition + new Vector3(0, bounceAmount, 0) * bounceAmplitude;
+        transform.localRotation = startingRotation * Quaternion.Euler(0, 0, bounceAmount * rockingAmplitude);
+    }
+}
